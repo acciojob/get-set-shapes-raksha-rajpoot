5f64f5d3390b69747a597bf852aee223eb79fc92
raksha-rajpoot <rakraj654@gmail.com>
2025-01-19 18:37:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,89 @@
 //complete this code
 // class Rectangle {}
+// class Rectangle {
+//   constructor(width, height) {
+//     if (width <= 0 || height <= 0) {
+//       throw new Error("Width and height must be positive integers.");
+//     }
+//     this._width = width;
+//     this._height = height;
+//   }
+
+ 
+//   get width() {
+//     return this._width;
+//   }
+
+ 
+//   get height() {
+//     return this._height;
+//   }
+
+ 
+//   getArea() {
+//     return this.width * this.height;
+//   }
+// }
+
+
+// class Square extends Animal {
+//   constructor(side) {
+//     if (side <= 0) {
+//       throw new Error("Side must be a positive integer.");
+//     }
+//     super(side, side); 
+//   }
+
+ 
+//   getPerimeter() {
+//     return 4 * this.width;
+//   }
+// }
+
+
+// // Do not change the code below this line
+// window.Rectangle = Rectangle;
+// window.Square = Square;
 class Rectangle {
   constructor(width, height) {
-    if (width <= 0 || height <= 0) {
-      throw new Error("Width and height must be positive integers.");
-    }
     this._width = width;
     this._height = height;
   }
 
- 
   get width() {
     return this._width;
   }
 
- 
   get height() {
     return this._height;
   }
 
- 
   getArea() {
-    return this.width * this.height;
+    return this._width * this._height;
   }
 }
 
-
-class Square extends Animal {
+class Square extends Rectangle {
   constructor(side) {
-    if (side <= 0) {
-      throw new Error("Side must be a positive integer.");
-    }
-    super(side, side); 
+    super(side, side);
+    this._side = side;
   }
 
- 
   getPerimeter() {
-    return 4 * this.width;
+    return 4 * this._side;
   }
 }
 
+// Example usage
+const rectangle = new Rectangle(5, 10);
+console.log(rectangle.width); 
+console.log(rectangle.height);
+console.log(rectangle.getArea());
+
+const square = new Square(7);
+console.log(square.width); 
+console.log(square.getArea()); 
+console.log(square.getPerimeter()); 
 
 // Do not change the code below this line
 window.Rectangle = Rectangle;
